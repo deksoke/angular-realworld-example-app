6c3fbb0c6aa7615ee405c30163cd40dd762fdc87
taywan <deksoke@gmail.com>
2017-05-31 16:16:21 +0700

    add forms pages

---
+++ src/app/forms/forms.module.ts
@@ -0,0 +1,28 @@
+import { NgModule, ModuleWithProviders } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { SharedModule } from '../shared';
+
+import { GeneralElementsComponent } from './general-elements/general-elements.component';
+import { AdvancedElementsComponent } from './advanced-elements/advanced-elements.component';
+import { EditorsComponent } from './editors/editors.component';
+
+const formsRouting: ModuleWithProviders = RouterModule.forChild([
+    { path: 'forms/general', component: GeneralElementsComponent },
+    { path: 'forms/advanced', component: AdvancedElementsComponent },
+    { path: 'forms/editors', component: EditorsComponent }
+]);
+
+@NgModule({
+  imports: [
+      CommonModule,
+      SharedModule,
+      formsRouting
+  ],
+  declarations: [
+      GeneralElementsComponent, 
+      AdvancedElementsComponent, 
+      EditorsComponent
+  ]
+})
+export class FormsModule { }
