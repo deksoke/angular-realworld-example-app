ea1c7e90ab27e83b9cb4d2a71e2a66554d6b76f8
taywan <deksoke@gmail.com>
2017-06-08 23:03:58 +0700

    add theme to assets, edit class form-control

--- src/app/tables/data-tables/data-tables.component.ts
+++ src/app/tables/data-tables/data-tables.component.ts
@@ -1,5 +1,10 @@
 import { Component, OnInit } from '@angular/core';
 
+export class dataModel {
+  name: string;
+  percent: number;
+}
+
 @Component({
   selector: 'app-data-tables',
   templateUrl: './data-tables.component.html',
@@ -7,9 +12,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class DataTablesComponent implements OnInit {
 
+  datas: dataModel[];
+
   constructor() { }
 
   ngOnInit() {
+    this.datas = [
+      { name: 'Update software', percent: 50 },
+      { name: 'Cleanup database', percent: 70 },
+      { name: 'Cron job running', percent: 30 },
+      { name: 'Fix and squish bugs', percent: 90 }
+    ];
+  }
+
+  setProgressStyle(data: dataModel){
+    let styles = {
+      width: data.percent.toString() + '%'
+    }
+    return styles;
+  }
+
+  setProgressClass(data: dataModel){
+    let classs = {
+      'progress-bar': true,
+      'progress-bar-danger': data.percent < 40,
+      'progress-bar-yellow': data.percent >= 40 && data.percent < 60,
+      'progress-bar-primary': data.percent >= 60 && data.percent < 80,
+      'progress-bar-success': data.percent >= 80
+    }
+    return classs;
+  }
+
+  setBadgeClass(data: dataModel){
+    let classs = {
+      'badge': true,
+      'bg-red': data.percent < 40,
+      'bg-yellow': data.percent >= 40 && data.percent < 60,
+      'bg-light-blue': data.percent >= 60 && data.percent < 80,
+      'bg-green': data.percent >= 80
+    }
+    return classs;
   }
 
 }
