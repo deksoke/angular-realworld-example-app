94ecb516b21c197b88d0eff1c0267c81cb4148c2
taywan <deksoke@gmail.com>
2017-06-13 17:41:15 +0700

    fix bug forms/advanced-form

--- src/app/forms/advanced-elements/advanced-elements.component.ts
+++ src/app/forms/advanced-elements/advanced-elements.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import * as moment from 'moment';
 
 interface ExampleSelectizeOption {
 	label: string;
@@ -41,6 +42,8 @@ const MULTI_SELECT_CONFIG:any = Object.assign({}, DEFAULT_DROPDOWN_CONFIG, {
 	maxItems: 5
 });
 
+
+declare var $:any;
 @Component({
   selector: 'app-advanced-elements',
   templateUrl: './advanced-elements.component.html',
@@ -59,6 +62,51 @@ export class AdvancedElementsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+
+	//Datemask dd/mm/yyyy
+    $("#datemask").inputmask("dd/mm/yyyy", {"placeholder": "dd/mm/yyyy"});
+    //Datemask2 mm/dd/yyyy
+    $("#datemask2").inputmask("mm/dd/yyyy", {"placeholder": "mm/dd/yyyy"});
+    //Money Euro
+    $("[data-mask]").inputmask();
+	
+	//Date picker
+    $('#datepicker').datepicker({
+      autoclose: true
+    });
+
+	//Date range picker
+    $('#reservation').daterangepicker();
+    //Date range picker with time picker
+    $('#reservationtime').daterangepicker({timePicker: true, timePickerIncrement: 30, format: 'MM/DD/YYYY h:mm A'});
+    //Date range as a button
+    $('#daterange-btn').daterangepicker(
+        {
+          ranges: {
+            'Today': [moment(), moment()],
+            'Yesterday': [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+            'Last 7 Days': [moment().subtract(6, 'days'), moment()],
+            'Last 30 Days': [moment().subtract(29, 'days'), moment()],
+            'This Month': [moment().startOf('month'), moment().endOf('month')],
+            'Last Month': [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+          },
+          startDate: moment().subtract(29, 'days'),
+          endDate: moment()
+        },
+        function (start, end) {
+          $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+        }
+    );
+
+	//Colorpicker
+    $(".my-colorpicker1").colorpicker();
+    //color picker with addon
+    $(".my-colorpicker2").colorpicker();
+
+    //Timepicker
+    $(".timepicker").timepicker({
+      showInputs: false
+    });
   }
 
   toggleEnabledState() {
