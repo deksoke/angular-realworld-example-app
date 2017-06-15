2e1c4120af5f805d2d9002f2424b28d994b7575c
taywan <deksoke@gmail.com>
2017-06-15 12:50:28 +0700

    fix form/editors page

--- src/app/forms/editors/editors.component.ts
+++ src/app/forms/editors/editors.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 
+declare var $:any, CKEDITOR:any;
+
 @Component({
   selector: 'app-editors',
   templateUrl: './editors.component.html',
@@ -10,6 +12,11 @@ export class EditorsComponent implements OnInit {
   constructor() { }
 
   ngOnInit() {
+    // Replace the <textarea id="editor1"> with a CKEditor
+    // instance, using default configuration.
+    CKEDITOR.replace('editor1');
+    //bootstrap WYSIHTML5 - text editor
+    $('#editor2').wysihtml5();
   }
 
 }
